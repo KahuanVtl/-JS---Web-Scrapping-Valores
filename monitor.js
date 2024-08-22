d61ae907a92998dcf264793ddeb4276b7163bf34
kahuan <kahuanvitelli@gmail.com>
2024-08-22 15:55:58 -0300

    Adicionando timer de 1 hora

--- monitor.js
+++ monitor.js
@@ -34,9 +34,13 @@ async function fetchHTML(url, selector) {
     }
 }
 
-fetchHTML(
-    'https://www.magazinevoce.com.br/magazineoficialmagaluweb/jogo-de-tacas-de-vidro-para-sobremesa-transparente-150ml-6-pecas-haus-concept-lotus/p/238303600/ud/tcsb/',
-    '#__next > div > main > section:nth-child(6) > div.sc-bCrHVQ.hJVwij > div > div > div > div > p'
-).then(value => {
-    console.log(value);
-});
+function checkValue() {
+    fetchHTML(
+        'https://www.magazinevoce.com.br/magazineoficialmagaluweb/jogo-de-tacas-de-vidro-para-sobremesa-transparente-150ml-6-pecas-haus-concept-lotus/p/238303600/ud/tcsb/',
+        '#__next > div > main > section:nth-child(6) > div.sc-bCrHVQ.hJVwij > div > div > div > div > p'
+    ).then(value => {
+        console.log(value);
+    });
+}
+
+setInterval(checkValue, 60 * 60 * 1000); //Timer de 1 hora
